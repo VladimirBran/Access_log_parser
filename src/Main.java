7ef5fe14f4f71e873f9e537fb77db5b970533168
Den Sen <stom-neft@mail.ru>
2025-02-09 16:41:28 +0500

    Add changes in new branch

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 import java.util.Scanner;
 
-import static java.awt.SystemColor.text;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введит етекст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длинна текста: " + text.length());
+        System.out.println("Введите первое число и нажмите <Enter>: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе числои нажмите <Enter>: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+        int sum = firstNumber + secondNumber; //Сумма чисел
+        System.out.println("Сумма: " + sum);
+        int difference = firstNumber - secondNumber; //Вычитание чисел
+        System.out.println("Разность: " + difference);
+        int product = firstNumber * secondNumber; //Умножение чисел
+        System.out.println("Произведение: " + product);
+        double quotient = (double) firstNumber / secondNumber; //Деление чисел
+        System.out.println("Частное: " + quotient);
+
     }
 }
