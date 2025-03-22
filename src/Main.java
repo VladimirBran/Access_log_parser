aa1db6aea644faafa12235514978f11d2514472e
Den Sen <stom-neft@mail.ru>
2025-03-22 18:44:14 +0500

    Add changes in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.File;
+import java.util.Scanner;
 
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+public class Finalhomework {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int validFileCount = 0; // Счетчик правильных файлов
+
+        while (true) {
+            System.out.print("Введите путь к файлу: ");
+            String path = scanner.nextLine();                //// Запрашиваем путь в консоли
+            File file = new File(path);
+            boolean fileExists = file.exists();   /// Переменная fileExists будет равна true, если файл существует, и false, если не существует.
+            boolean isDirectory = file.isDirectory(); // является ли указанный путь путём именно к файлу, а не к папке.
+
+            if (!fileExists || isDirectory) {
+                System.out.println("Файл не существует. Попробуйте снова.");
+                continue;
+            }
+
+            validFileCount++;
+            System.out.println("Путь указан верно. Это файл номер " + validFileCount);
+        }
     }
 }
+
