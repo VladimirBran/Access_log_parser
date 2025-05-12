36172bea49086a8eb1faee7d67484dde19b22729
Denis Sen <100854957+VladimirBran@users.noreply.github.com>
2025-05-12 19:28:41 +0500

    Delete src/CourseWorkEx2 directory

--- src/CourseWorkEx2/LongLineException.java
+++
@@ -1,8 +0,0 @@
-package CourseWorkEx2;
-
-public class LongLineException extends RuntimeException {          /// /класс исключение для строки больше 1024 символов
-    public LongLineException(String message) {
-        super(message);
-    }
-}
-
